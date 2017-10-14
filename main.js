bd911c618910380b8165223a5fbd1af8fcdecc9a
Walter Ulreich <walterulreich@Walters-MacBook-Air.local>
2017-10-14 14:39:35 -0600

    commit portfolio files

---
+++ main.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+  var offset = 70
+
+$('#about-link').click(function(event) {
+  event.preventDefault()
+  $($(this).attr('href'))[0].scrollIntoView()
+  scrollBy(0, -offset)
+});
+
+  var offset2 = 90
+
+  $('#portfolio-link').click(function(event) {
+  event.preventDefault()
+  $($(this).attr('href'))[0].scrollIntoView()
+  scrollBy(0, -offset2)
+  });
+
+  var offset3 = 30
+
+  $('#contact-link').click(function(event) {
+  event.preventDefault()
+  $($(this).attr('href'))[0].scrollIntoView()
+  scrollBy(0, -offset3)
+  });
+
+})
