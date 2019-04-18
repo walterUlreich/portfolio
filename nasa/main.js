6c3f46d56cbc3b10ce23e9a1ba9e7e33e0bdca8a
Walter Ulreich <walterulreich@Walters-MacBook-Air.local>
2019-04-18 13:20:49 -0500

    vueify nasa

--- nasa/main.js
+++ nasa/main.js
@@ -1,40 +1,24 @@
-class UI {
-  paint(asteroids) {
-    const tableBody = document.getElementById('table-body')
-    tableBody.innerHTML = ''
+const app = new Vue({
+  el: '#app',
+  data: {
+    date: '',
+    asteroidArray: [],
+    showTable: false,
+  },
+  methods: {
+    findAsteroids: function() {
+      fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.date}&end_date=${this.date}&api_key=oVYuYjmTXpNRtt3f4oXMdm8EJGoybo73QLxLycCE`)
+        .then(res => res.json())
+        .then(data => {
 
-    asteroids.forEach((asteroid) => {
+          this.asteroidArray = data["near_earth_objects"][this.date]
 
-      let html = `
-        <tr class="${asteroid.is_potentially_hazardous_asteroid}">
-          <th scope="row">${asteroid.id}</th>
-          <td>${asteroid.name}</td>
-          <td>${asteroid.estimated_diameter.feet.estimated_diameter_max}</td>
-          <td>${asteroid.close_approach_data[0]["relative_velocity"]["miles_per_hour"]}</td>
-          <td>${asteroid.close_approach_data[0]["miss_distance"]["miles"]}</td>
-          <td>${asteroid.is_potentially_hazardous_asteroid}</td>
-        </tr>
-      `
+          console.log(this.asteroidArray)
+          this.showTable = true
 
-      tableBody.innerHTML += html
-    })
-  }
-}
-
-
-
-
-document.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const asteroidDate = document.getElementById('date').value
-
-  fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${asteroidDate}&end_date=${asteroidDate}&api_key=oVYuYjmTXpNRtt3f4oXMdm8EJGoybo73QLxLycCE`)
-    .then(res => res.json())
-    .then(data => {
-      let asteroidArray = data["near_earth_objects"][asteroidDate]
-      const ui = new UI
-      ui.paint(asteroidArray)
-    })
-    .catch(err => console.log(err))
-})
+        })
+        .catch(err => console.log(err))
+    },
+  },
+  computed: {},
+}).$mount('#app')
